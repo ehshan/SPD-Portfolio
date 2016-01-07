bb154dc96d8041e30fa797fab8bc62402a11dfc7
Ehshan Veerabangsa <ehshan@msn.com>
2016-01-07 22:41:48 +0000

    updated driver

--- exercises/week01/src/Driver.java
+++ exercises/week01/src/Driver.java
@@ -3,10 +3,28 @@
  */
 public class Driver {
 
-    public static void main (String[]args){
-
+    public static void main(String[] args) {
         Storage<BankAccount> aStorage = new Storage<>();
         Storage<String> sStorage = new Storage<>();
 
+        Class<BankAccount> baCls = BankAccount.class;
+        BankAccount myAccount = null;
+        try {
+            myAccount = baCls.newInstance();
+            aStorage.setValue(myAccount);
+
+            // Deposit
+            myAccount.deposit(15);
+        } catch (InstantiationException | IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(aStorage.getValue().showBalance());
+
+        if (aStorage.getClass() == aStorage.getClass()) {
+            System.out.println("EQUAL");
+        } else {
+            System.out.println("NOT EQUAL");
+        }
     }
 }
