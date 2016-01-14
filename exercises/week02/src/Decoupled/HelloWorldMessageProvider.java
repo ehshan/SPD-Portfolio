26dfb67804d688c4d3bf257140bbe4bd807c28bb
Ehshan Veerabangsa <ehshan@msn.com>
2016-01-14 22:01:56 +0000

    added helloworld decoupled

---
+++ exercises/week02/src/Decoupled/HelloWorldMessageProvider.java
@@ -0,0 +1,13 @@
+package Decoupled;
+
+/**
+ * Created by ehshan on 14/01/2016.
+ */
+public class HelloWorldMessageProvider {
+
+    private String message = "Hello World!";
+
+    public String getMessage() {
+        return message;
+    }
+}
---
+++ exercises/week02/src/Decoupled/StandardOutMessageRenderer.java
@@ -0,0 +1,26 @@
+package Decoupled;
+
+/**
+ * Created by ehshan on 14/01/2016.
+ */
+public class StandardOutMessageRenderer {
+
+    private HelloWorldMessageProvider messageProvider = null;
+
+    public void render() {
+        if (messageProvider == null) {
+            throw new RuntimeException(
+                    "You must set the property messageProvider of class:"
+                            + StandardOutMessageRenderer.class.getName());
+        }
+        System.out.println(messageProvider.getMessage());
+    }
+
+    public HelloWorldMessageProvider getMessageProvider() {
+        return this.messageProvider;
+    }
+
+    public void setMessageProvider(HelloWorldMessageProvider provider) {
+        this.messageProvider = provider;
+    }
+}
