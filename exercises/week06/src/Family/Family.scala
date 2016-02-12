174a84a698934f9c5d8e1938ac03240b375f8e1d
Ehshan Veerabangsa <ehshan@msn.com>
2016-02-12 22:48:23 +0000

    added class arguments exercise - family

---
+++ exercises/week06/src/Family/Family.scala
@@ -0,0 +1,23 @@
+package Family
+
+import com.atomicscala.AtomicTest._
+
+class Family(names : String*) {
+
+  def familySize(): Int ={
+    var count = 0
+    for(s <- names){
+      count += 1
+    }
+    count
+  }
+}
+
+object TestArgs extends App {
+
+  val family1 = new Family("Mum", "Dad", "Sally", "Dick")
+  family1.familySize() is 4
+
+  val family2 = new Family("Dad", "Mom", "Harry")
+  family2.familySize() is 3
+}
