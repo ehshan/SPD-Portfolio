56be85273e4cf67c615fa4fd021f631c17b3d33b
Ehshan Veerabangsa <ehshan@msn.com>
2016-02-26 20:47:39 +0000

    updated execute and toString methods

--- cw-one/src/sml/SubInstruction.java
+++ cw-one/src/sml/SubInstruction.java
@@ -25,6 +25,13 @@ public class SubInstruction  extends Instruction{
 
     @Override
     public void execute(Machine m) {
+        int value1 = m.getRegisters().getRegister(op1);
+        int value2 = m.getRegisters().getRegister(op2);
+        m.getRegisters().setRegister(result, value1 - value2);
+    }
 
+    @Override
+    public String toString() {
+        return super.toString() + " " + op1 + " + " + op2 + " to " + result;
     }
 }
