35573955a8c677cec8ecbe8a967ef00c8716519f
Ehshan Veerabangsa <ehshan@msn.com>
2016-02-26 21:32:26 +0000

    added execute test

--- cw-one/src/Tests/SubInstructionTest.java
+++ cw-one/src/Tests/SubInstructionTest.java
@@ -11,22 +11,31 @@ import static org.junit.Assert.*;
  */
 public class SubInstructionTest {
 
+    Machine machine = new Machine();
+    Registers registers = new Registers();
     SubInstruction instruction = new SubInstruction("sub", 1, 3, 2);
 
     @org.junit.Before
     public void setUp() throws Exception {
-
+        machine.setRegisters(registers);
 
     }
 
     @org.junit.After
     public void tearDown() throws Exception {
+        machine = null;
+        registers = null;
         instruction = null;
     }
 
     @org.junit.Test
     public void testExecute() throws Exception {
 
+        machine.getRegisters().setRegister(3, 3);
+        machine.getRegisters().setRegister(2, 2);
+        instruction.execute(machine);
+
+        assertTrue(machine.getRegisters().getRegister(1) == 1);
     }
 
     @org.junit.Test
