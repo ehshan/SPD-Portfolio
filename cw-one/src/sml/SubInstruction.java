5ebba381eda1fb69019fd55e1276d61142ad4e28
Ehshan Veerabangsa <ehshan@msn.com>
2016-02-26 21:27:32 +0000

    correct error in toString

--- cw-one/src/sml/SubInstruction.java
+++ cw-one/src/sml/SubInstruction.java
@@ -32,6 +32,6 @@ public class SubInstruction  extends Instruction{
 
     @Override
     public String toString() {
-        return super.toString() + " " + op1 + " + " + op2 + " to " + result;
+        return super.toString() + " " + op1 + " - " + op2 + " to " + result;
     }
 }
