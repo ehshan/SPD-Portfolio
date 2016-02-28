320e96ae1d56fc9887ae74715d3e7d33ae633cb4
Ehshan Veerabangsa <ehshan@msn.com>
2016-02-28 16:02:51 +0000

    added mul execute test

--- cw-one/src/Tests/MulInstructiTest.java
+++ cw-one/src/Tests/MulInstructiTest.java
@@ -4,7 +4,9 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import sml.Machine;
 import sml.MulInstruction;
+import sml.Registers;
 
 import static org.junit.Assert.*;
 
@@ -13,20 +15,31 @@ import static org.junit.Assert.*;
  */
 public class MulInstructiTest {
 
+    Machine machine = new Machine();
+    Registers registers = new Registers();
     MulInstruction instruction = new MulInstruction("mul",1,2,3);
+
 
     @Before
     public void setUp() throws Exception {
-
+        machine.setRegisters(registers);
     }
 
     @After
     public void tearDown() throws Exception {
-
+        machine = null;
+        registers = null;
+        instruction = null;
     }
 
     @Test
     public void testExecute() throws Exception {
+
+        machine.getRegisters().setRegister(2,2);
+        machine.getRegisters().setRegister(3,3);
+        instruction.execute(machine);
+
+        assertTrue(machine.getRegisters().getRegister(1) == 6);
 
     }
 
